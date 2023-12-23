271794cef41a3d3be9fdc3ef90854487cf5c26ca
Boryan <77213556+plxr1awsbl@users.noreply.github.com>
2023-12-23 17:53:08 +0700

    Initial commit

--- estates/settings.py
+++ estates/settings.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
-    'main',
     'contacts',
 ]
 
--- main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from catalog.models import Property
-
-# Create your views here.
-def index(request):
-    
-    return render(request, 'main/index.html')
