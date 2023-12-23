5ec91a21068bc9265b14c9faad8941d4263c7621
Boryan <77213556+plxr1awsbl@users.noreply.github.com>
2023-12-23 17:08:30 +0700

    Main page replace
    Main page is catalog now

--- estates/urls.py
+++ estates/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('catalog/', include('catalog.urls')),
+    path('', include('catalog.urls')),
     path('contacts/', include('contacts.urls'))
 ]
 
